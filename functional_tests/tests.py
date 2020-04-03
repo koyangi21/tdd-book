1e363cea1a10920796ea5e70fe63955dd3d92d7c
koyangi <koyangi@localhost.localdomain>
2020-04-03 13:51:49 -0400

    first septs of FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -99,4 +99,29 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn('kill all humans',page_text)
         self.assertIn('beg for food',page_text)
 
+    def test_layout_and_styling(self):
+        #Bbanggu goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024,768)
+
+        #He notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width']/2,
+            512,
+            delta=10
+        )
+
+        #He starts a new list and sees the input is 
+        #nicely centered there as well
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width']/2,
+            512,
+            delta=10
+        )
+
         #satisfied he makes some noppers too
