99d90222ae44d37bf51c27d72362d08b3e046b28
koyangi <koyangi@localhost.localdomain>
2020-04-21 17:19:08 -0400

    Custom passwordless auth backend + custom user model

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,5 +1,4 @@
-window.Superlists = {};
-window.Superlists.initialize = function () {
+var initialize = function () {
 	$('input[name="text"]').on('keypress', function () {
 		$('.has-error').hide();
 	});
