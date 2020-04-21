46e97ea3f5bb523a416fac87d8e3825f10dda8ab
koyangi <koyangi@localhost.localdomain>
2020-04-21 12:40:28 -0400

    Custom passwordless auth backend + custom user model

--- superlists/settings.py
+++ superlists/settings.py
@@ -33,7 +33,11 @@ else:
 	ALLOWED_HOSTS = []
 
 ##ALLOWED_HOSTS = ['*']
-
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'ninjadev1971@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
 # Application definition
 
@@ -45,6 +49,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'lists',
+    'accounts',
+]
+
+AUTH_USER_MODEL = 'accounts.User'
+AUTHENTICATION_BACKENDS = [
+    'accounts.authentication.PasswordlessAuthenticationBackend'
 ]
 
 MIDDLEWARE = [
